9b37648b5ac7f88a9d1ed863349780a3aac2232e
rajvendrasingh <sseema7350@gmail.com>
2026-01-15 14:38:50 +0530

    Update event.js

--- event.js
+++ event.js
@@ -1,20 +1,21 @@
 
 bg=document.querySelector("div")
-let back="red"
-// div=document.querySelector('div')
+let back="red";
+// div=document.querySelector('div');
 bg.addEventListener('mouseover',()=>{
     if( back==='red'){
-        back="yellow"
-bg.classList.add('yellow')
-bg.classList.remove('red')
+        back="yellow";
+bg.classList.add('yellow');
+bg.classList.remove('red');
 
     }
     else{
-        back="red"
-        bg.classList.add('red')
-bg.classList.remove('yellow')
+        back="red";
+        bg.classList.add('red');
+bg.classList.remove('yellow');
 
     }
-    console.log(back)
+    console.log(back);
 })
+
 
